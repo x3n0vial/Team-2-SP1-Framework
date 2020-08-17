41ea1ea332b3b691e5222992d7e7481d1d9745b3
191024Q@mymail.nyp.edu.sg <191024Q@mymail.nyp.edu.sg>
2020-08-17 14:17:03 +0800

    hii

---
+++ SP1Framework/Enemy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Damage.h"
+#include <iostream>
+using namespace std;
+
+class Enemy : public Damage
+{
+private:
+	short HP, Attack;
+
+public:
+	Enemy(string Entity);
+	~Enemy();
+	void setEnemy(int h, int d);
+	short getEnemy();
+	void Attack();
+};
